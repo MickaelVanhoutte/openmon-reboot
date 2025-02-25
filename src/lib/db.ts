51af217a368aa7390583740e2d10bed50400efa8
Perso <perso@MacBook-Pro-de-MICKAEL.local>
2025-02-25 21:52:03 +0100

    feat: indexdb saves

---
+++ src/lib/db.ts
@@ -0,0 +1,49 @@
+import Dexie, { type EntityTable, type Observable } from 'dexie';
+import { liveQuery } from "dexie";
+
+
+interface Save {
+    id?: number;
+    created: number;
+    updated: number;
+}
+
+const db = new Dexie('Save') as Dexie & {
+  saves: EntityTable<
+  Save,
+    'id' // primary key "id" (for the typings only)
+  >;
+};
+
+// Schema declaration:
+db.version(1).stores({
+  saves: '++id, created, updated' // primary key "id" (for the runtime!)
+});
+
+export type { Save };
+export { db };
+
+
+export async function createSave(save: Save) {
+    try {
+      const id = await db.saves.add(save);
+
+      console.log(`Save successfully added. Got id ${id}`);
+    } catch (error) {
+     console.log(`Failed to add save: ${error}`);
+    }
+}
+
+export function getSaves(): Observable<Save[]> {
+    return liveQuery(
+            () => db.saves.toArray()
+          );
+}
+
+export function updateSave(save: Save) {
+    db.saves.put(save);
+}
+
+export function deleteSave(id: number) {
+    db.saves.delete(id);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import { decodeJwtResponse, onGoogleScriptLoad, getUser } from '$lib';
+    import { getSaves, createSave, deleteSave } from '$lib/db';
+    import type { Save } from '$lib/db';
 
-    let user = null;
-  
+    let saves: Save[] = [];
+
+    const newGame = () => {
+        createSave({created: Date.now(), updated: Date.now()});
+        updateSaves();
+    };
+
+    function updateSaves(){
+        getSaves().subscribe(
+            value => {
+                saves = value;
+            }
+        );
+    }
+
+    function deleteS(id: number){    
+        deleteSave(id);
+        updateSaves();
+    }
+
     onMount(() => {
-        onGoogleScriptLoad(decodeJwtResponse);
-        user = getUser();
+        updateSaves();
     });
 </script>
 
-{#if user}
-    <p>Logged in as {user.email}</p>
+{#if saves?.length > 0}
+
+<div class="flex flex-col items-center justify-center">
+    <h1 class="text-3xl font-bold">Your Saves</h1>
+    <ul class="flex flex-col items-center justify-center">
+        {#each saves as save}
+            <li class="flex flex-row items-center justify-center">
+                <a href={`/${save.id}`} class="text-blue-500 underline">{save.id}</a>
+                <button class="text-red-500" on:click={() => deleteS(save.id as number)}>Delete</button>
+            </li>
+        {/each}
+    </ul>
+</div>
+
 {:else}
 
-<div id="googleSignIn"></div>
+<div class="flex flex-col items-center justify-center">
+    <h1 class="text-3xl font-bold">No Saves</h1>
+    <button class="text-blue-500 underline" on:click={newGame}>Start a new game !</button>
+</div>
+
 
 {/if}
