a84dabdde6d10c8d64a093a0958a30a30e9e4057
Mickael Vanhoutte <mickael.vanhoutte@leroymerlin.fr>
2025-03-08 15:43:13 +0100

    fix: saves ref

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { getSaves, createSave, deleteSave } from '$lib/db';
-  import type { Save } from '$lib/db';
+  //import { getSaves, createSave, deleteSave } from '$lib/db';
+  //import type { Save } from '$lib/db';
 
-  let saves: Save[] = [];
+  let saves= [];
 
-  const newGame = () => {
-    createSave({ created: Date.now(), updated: Date.now() });
-    updateSaves();
-  };
+  // const newGame = () => {
+  //   createSave({ created: Date.now(), updated: Date.now() });
+  //   updateSaves();
+  // };
 
-  function updateSaves() {
-    getSaves().subscribe((value) => {
-      saves = value;
-    });
-  }
+  // function updateSaves() {
+  //   getSaves().subscribe((value) => {
+  //     saves = value;
+  //   });
+  // }
 
-  function deleteS(id: number) {
-    deleteSave(id);
-    updateSaves();
-  }
+  // function deleteS(id: number) {
+  //   deleteSave(id);
+  //   updateSaves();
+  // }
 
-  onMount(() => {
-    updateSaves();
-  });
+  // onMount(() => {
+  //   updateSaves();
+  // });
 </script>
 
 {#if saves?.length > 0}
@@ -33,7 +33,8 @@
       {#each saves as save}
         <li class="flex flex-row items-center justify-center">
           <a href={`/${save.id}`} class="text-blue-500 underline">{save.id}</a>
-          <button class="text-red-500" on:click={() => deleteS(save.id as number)}>Delete</button>
+          <button class="text-red-500" >Delete</button>
+          <!-- on:click={() => deleteS(save.id as number)} -->
         </li>
       {/each}
     </ul>
@@ -41,6 +42,7 @@
 {:else}
   <div class="flex flex-col items-center justify-center">
     <h1 class="text-3xl font-bold">No Saves</h1>
-    <button class="text-blue-500 underline" on:click={newGame}>Start a new game !</button>
+    <button class="text-blue-500 underline">Start a new game !</button>
+    <!--  on:click={newGame} -->
   </div>
 {/if}
